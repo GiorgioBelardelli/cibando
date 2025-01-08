04eb0cada6e2179924d9b32bc2d97deac7584ee8
GiorgioBelardelli <asterapocalypse@gmail.com>
2025-01-08 14:56:02 +0100

    Scaffholding + import di varie librerie + carosello di bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,29 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  standalone: false,
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  standalone: false
 })
 export class AppComponent {
   title = 'cibando';
+  path = "../assets/images/carousel-";
+  images = [
+    {
+      id: 1,
+      label: 'Spaghetti al pomodoro',
+
+    },
+    {
+      id: 2,
+      label: 'Tagliata di manzo',
+    },
+    {
+      id: 3,
+      label: 'Tiramisù classico',
+    }
+
+  ];
+
+  allievi = ['Davide', 'Francesco', 'Maria Sole', 'Gioele' ]
+
 }
